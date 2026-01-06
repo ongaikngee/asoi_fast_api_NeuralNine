8c05ba2c2a445ca3b308712a59a1eb88e515342a
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-01-06 08:39:31 +0800

    get all and get by id

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI
+from typing import Optional
+
 
 api = FastAPI()
+
+all_todos = [
+    {"todo_id": 1, "todo_name": "Sports", "todo_description": "Go to the gym"},
+    {
+        "todo_id": 2,
+        "todo_name": "Study",
+        "todo_description": "Read FastAPI documentation",
+    },
+    {
+        "todo_id": 3,
+        "todo_name": "Work",
+        "todo_description": "Finish API endpoint implementation",
+    },
+    {
+        "todo_id": 4,
+        "todo_name": "Groceries",
+        "todo_description": "Buy fruits and vegetables",
+    },
+    {
+        "todo_id": 5,
+        "todo_name": "Relax",
+        "todo_description": "Watch a movie in the evening",
+    },
+]
 
 
 # GET, POST, PUT, DELETE
@@ -9,3 +35,16 @@ def index():
     return {"message": "Hello World"}
 
 
+@api.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in all_todos:
+        if todo["todo_id"] == todo_id:
+            return {"result": todo}
+
+
+@api.get("/todos")
+def get_todos(first_n: Optional[int] = None):
+    if first_n:
+        return all_todos[:first_n]
+    else:
+        return all_todos
